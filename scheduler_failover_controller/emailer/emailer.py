4bada87ae43eda19873612e5b68da962e7a7693e
rssanders3 <rssanders3@gmail.com>
2016-11-21 14:49:17 -0700

    Fixing issue with Emailer and adding debug statements

--- scheduler_failover_controller/emailer/emailer.py
+++ scheduler_failover_controller/emailer/emailer.py
@@ -1,21 +1,21 @@
 import importlib
 from airflow import configuration
 
+EMAIL_SUBJECT = "Airflow Alert - Scheduler Failover Controller Failed to Startup Scheduler"
+EMAIL_BODY = """
+The Scheduler Failover Controller failed to Restart the Scheduler on all Scheduler Hosts.<br/>
+<br/>
+Host Name: {0}<br/>
+Retry Count: {1}<br/>
+Latest Message from trying to get Status: {2}<br/>
+Latest Message from trying to Restart: {3}<br/>
+"""
+
 
 class Emailer:
 
-    EMAIL_SUBJECT = "Airflow Alert - Scheduler Failover Controller Failed to Startup Scheduler"
-    EMAIL_BODY = """
-    The Scheduler Failover Controller failed to Restart the Scheduler on all Scheduler Hosts.
-
-    Host Name: {0}
-    Retry Count: {1}
-    Latest Message from trying to get Status: {2}
-    Latest Message from trying to Restart: {3}
-    """
-
     def __init__(self, alert_to_email, logger):
-        logger.debug("Creating Emailer with Args: {'alert_to_email':'" + str(alert_to_email) + "', 'logger':'" + str(logger) + "'}")
+        logger.debug("Creating Emailer with Args - alert_to_email: {alert_to_email}, logger: {logger}".format(**locals()))
         self.alert_to_email = alert_to_email
         self.logger = logger
         path, attr = configuration.get('email', 'EMAIL_BACKEND').rsplit('.', 1)
@@ -25,17 +25,18 @@ class Emailer:
             self.logger.warn("alert_to_email value isn't provided. The email will not be able to send alert emails.")
 
     def send_alert(self, current_host, retry_count, latest_status_message, latest_start_message):
+        self.logger.debug("Sending Email with Args - current_host: {current_host}, retry_count: {retry_count}, latest_status_message: {latest_status_message}, latest_start_message: {latest_start_message}".format(**locals()))
         if self.alert_to_email:
             try:
                 self.email_backend(
                     self.alert_to_email,
-                    self.EMAIL_SUBJECT,
-                    self.EMAIL_BODY.format(current_host, retry_count, latest_status_message, latest_start_message),
+                    EMAIL_SUBJECT,
+                    EMAIL_BODY.format(current_host, retry_count, latest_status_message, latest_start_message),
                     files=None,
                     dryrun=False
                 )
                 self.logger.info('Alert Email Sent Sent')
             except Exception as e:
-                self.logger.critical('Failed to Send Email: ' + str(e), 'error')
+                self.logger.critical('Failed to Send Email: ' + str(e) + 'error')
         else:
             self.logger.critical("Couldn't send email since the alert_to_email config is not set")
