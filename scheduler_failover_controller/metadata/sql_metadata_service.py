e4da827b530199c470148bc1a862bdf93fbaf016
Ubuntu <sshuser@hn0-aristo.s2y1flwmcywennuvk3encip5mh.gx.internal.cloudapp.net>
2019-01-30 23:55:29 +0000

    Made the failover scheduler more resilient

--- scheduler_failover_controller/metadata/sql_metadata_service.py
+++ scheduler_failover_controller/metadata/sql_metadata_service.py
@@ -3,6 +3,7 @@ from scheduler_failover_controller.utils import date_utils
 from sqlalchemy import create_engine, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import time
 
 import datetime
 
@@ -26,6 +27,7 @@ class SQLMetadataService(BaseMetadataService):
         engine_args = {}
         self.engine = create_engine(sql_alchemy_conn, **engine_args)
         self.Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=self.engine))
+        self.query_tries = 5
 
     def initialize_metadata_source(self):
         # Creating metadata table. If the creation fails assume it was created already.
@@ -37,65 +39,66 @@ class SQLMetadataService(BaseMetadataService):
             self.logger.info("Table might already exist. Suppressing Exception.")
 
     def get_failover_heartbeat(self):
-        session = self.Session()
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "failover_heartbeat").first()
-        if entry is not None:
-            heart_beat_date_str = entry.value
+        heart_beat_date_str = self.resilient_get_query("failover_heartbeat", self.query_tries)
+        if heart_beat_date_str is not None:
             heart_beat_date = date_utils.get_string_as_datetime(heart_beat_date_str)
         else:
             heart_beat_date = None
-        session.close()
         return heart_beat_date
 
     def set_failover_heartbeat(self):
-        session = self.Session()
         heart_beat_date_str = date_utils.get_datetime_as_str(datetime.datetime.now())  # get current datetime as string
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "failover_heartbeat").first()
-        if entry is not None:
-            entry.value = heart_beat_date_str
-        else:
-            session.add(SQLMetadata(key="failover_heartbeat", value=heart_beat_date_str))
-        session.commit()
-        session.close()
+	self.resilient_set_query("failover_heartbeat", heart_beat_date_str, self.query_tries)
 
     def get_active_failover_node(self):
-        session = self.Session()
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "active_failover_node").first()
-        session.close()
-        if entry is not None:
-            return entry.value
-        else:
-            return None
+        return self.resilient_get_query("active_failover_node", self.query_tries)
 
     def set_active_failover_node(self, node):
-        session = self.Session()
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "active_failover_node").first()
-        if entry is not None:
-            entry.value = node
-        else:
-            session.add(SQLMetadata(key="active_failover_node", value=node))
-        session.commit()
-        session.close()
+	self.resilient_set_query("active_failover_node", node, self.query_tries)
 
     def get_active_scheduler_node(self):
-        session = self.Session()
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "active_scheduler_node").first()
-        session.close()
-        if entry is not None:
-            return entry.value
-        else:
-            return None
+        return self.resilient_get_query("active_scheduler_node", self.query_tries)
 
     def set_active_scheduler_node(self, node):
-        session = self.Session()
-        entry = session.query(SQLMetadata).filter(SQLMetadata.key == "active_scheduler_node").first()
-        if entry is not None:
-            entry.value = node
-        else:
-            session.add(SQLMetadata(key="active_scheduler_node", value=node))
-        session.commit()
-        session.close()
+        self.resilient_set_query("active_scheduler_node", node, self.query_tries)
 
+    def resilient_get_query(self, key, tries):
+        session = self.Session()
+        try:
+            entry = session.query(SQLMetadata).filter(SQLMetadata.key == key).first()
+            if entry is not None:
+                return entry.value
+            else:
+                return None
+        except:
+            if(tries > 1):
+                print("Failed to execute get query for ", key, " Retrying ...")
+		time.sleep(5)
+                self.resilient_get_query(key, tries - 1)
+            else:
+                raise
+        finally:
+            session.close()
+
+    def resilient_set_query(self, key, val, tries):
+        session = self.Session()
+        try:
+            entry = session.query(SQLMetadata).filter(SQLMetadata.key == key).first()
+            if entry is not None:
+                entry.value = val
+            else:
+                session.add(SQLMetadata(key=key, value=val))
+            session.commit()
+        except:
+            if(tries > 1):
+                print("Failed to execute set query for ", key ," Retrying ...")
+		time.sleep(5)
+                self.resilient_set_query(key, val, tries - 1)
+            else:
+                raise
+        finally:
+            session.close()
+ 
     def clear(self):
         session = self.Session()
         session.query(SQLMetadata).delete()
